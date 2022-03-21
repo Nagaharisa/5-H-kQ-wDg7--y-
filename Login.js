c83ede4985578045e5063e50c025e0ae85e4cd30
Alex <99165584+Nagaharisa@users.noreply.github.com>
2022-03-21 09:50:20 +0200

    Update Login.js

--- Login.js
+++ Login.js
@@ -3,13 +3,12 @@ let reg = 0;
 document.addEventListener("DOMContentLoaded", onkoKirjautunut);
 
 async function häsh(häshättävä) {
-    const res = await fetch(`https://api.hashify.net/hash/md5/hex?value=${häshättävä}`)
-    const data = await res.json()
-    return data.Digest
+    const res = await fetch(`https://api.hashify.net/hash/md5/hex?value=${häshättävä}`);
+    const data = await res.json();
+    return data.Digest;
 }
 
 async function onkoKirjautunut() {
-    
     let kirjautunut = localStorage.getItem("kirjautunut");
 
     if (kirjautunut) {
@@ -18,16 +17,12 @@ async function onkoKirjautunut() {
 }
 
 async function kirjaudu() {
-    if (
-        document.getElementById("nimi").value == "" ||
-        document.getElementById("salasana").value == ""
-    ) {
+    if (document.getElementById("nimi").value == "" || document.getElementById("salasana").value == "") {
         alert("Anna nimi ja salasana!");
         return;
     }
 
-    if (document.getElementById("nimi").value == localStorage.getItem("nimi") &&
-        await häsh(document.getElementById("salasana").value) == localStorage.getItem("salasana")) {
+    if (document.getElementById("nimi").value == localStorage.getItem("nimi") && (await häsh(document.getElementById("salasana").value)) == localStorage.getItem("salasana")) {
         window.location.href = "kotisivu.html";
     } else {
         alert("Nimi tai salasana on väärin!");
@@ -39,20 +34,15 @@ async function rekisteröidy() {
     document.getElementById("kirjautumisNappi").style.display = "none";
 
     if (reg == 1) {
-        if (
-            document.getElementById("nimi").value != "" &&
-            document.getElementById("salasana").value != ""
-        ) {
+        if (document.getElementById("nimi").value != "" && document.getElementById("salasana").value != "") {
             localStorage.setItem("nimi", document.getElementById("nimi").value);
-            localStorage.setItem(
-                "salasana",
-                await häsh(document.getElementById("salasana").value)
-            );
+            localStorage.setItem("salasana", await häsh(document.getElementById("salasana").value));
             localStorage.setItem("kirjautunut", true);
             window.location.href = "kotisivu.html";
         } else {
             alert("Anna nimi ja salasana");
         }
     }
+    
     reg = 1;
 }
